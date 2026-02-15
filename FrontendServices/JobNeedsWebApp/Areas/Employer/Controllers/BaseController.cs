918189b17e808fe65ceb8e7ba6e335d86639f35a
Garikipati SudhakaraRao <garikipati.sudhakarrao@gmail.com>
2026-02-16 04:29:37 +0530

    Included Admin and Employer areas and shared files.

---
+++ FrontendServices/JobNeedsWebApp/Areas/Employer/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobNeedsWebApp.Areas.Employer.Controllers
+{
+    [Area("Employer")]
+    public class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
